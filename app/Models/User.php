7e1ebfb43d2f0707e0e070cb1cc05060831ec343
vagrant <vagrant@homestead>
2018-05-06 17:54:50 +0000

    Add user statuses

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,9 +44,19 @@ class User extends Authenticatable
             $user->activation_token = str_random(30);
         });
     }
-	
+
 	public function sendPasswordResetNotification($token)
     {
         $this->notify(new ResetPassword($token));
+    }
+
+    public function statuses()
+    {
+        return $this->hasMany(status::class);
+    }
+
+    public function feed()
+    {
+        return $this->statuses()->orderBy('created_at', 'desc');
     }
 }
